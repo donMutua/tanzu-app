c53b6edd51c06af78c3ae2590fbe55676983b061
donMutua <don@wefarm.com>
2022-08-22 18:35:10 +0300

    Add favicon test

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -3,10 +3,14 @@ import Footer from "@/components/footer/Footer";
 import { Community } from "@/components/community/Community";
 import { Hero } from "../../components/hero/Hero";
 import { WaitListSubscribe } from "@/components/mailChimpSubscribe/WaitListSubscribe";
+import Head from "next/head";
 
 export function HomeScreen() {
   return (
     <>
+      <Head>
+        <title>Tanzu apprenticeship</title>
+      </Head>
       <Hero />
       <About />
       <Community />
